8276a0c6860423942942fb3c0f8d93917b76e4c8
deep kukreja <deepkukreja001@gmail.com>
2026-02-03 21:10:25 +0530

    Add files via upload

---
+++ ObjectOrientedProgramming/AtmMachineLogic.py
@@ -0,0 +1,65 @@
+class ATM:
+    def __init__(self):
+        self.pin = ''
+        self.balance = 0
+        self.menu()
+
+    def menu(self):
+        while True:
+            user_input = input("""
+Hello, how would you like to proceed?
+1. Enter 1 to create pin
+2. Enter 2 to deposit
+3. Enter 3 to withdraw
+4. Enter 4 to check balance
+5. Enter 5 to exit
+""")
+
+            if user_input == "1":
+                self.create_pin()
+            elif user_input == "2":
+                self.deposit()
+            elif user_input == "3":
+                self.withdraw()
+            elif user_input == "4":
+                self.check_balance()
+            elif user_input == "5":
+                print("Bye")
+                break
+            else:
+                print("Invalid choice")
+
+    def create_pin(self):
+        self.pin = input("Enter your pin: ")
+        print("Pin set successfully")
+
+    def deposit(self):
+        temp_pin = input("Enter your pin: ")
+        if temp_pin == self.pin:
+            amount = int(input("Enter the amount: "))
+            self.balance += amount
+            print("Deposit successful")
+        else:
+            print("Invalid pin")
+
+    def withdraw(self):
+        temp_pin = input("Enter your pin: ")
+        if temp_pin == self.pin:
+            amount = int(input("Enter the amount: "))
+            if amount <= self.balance:
+                self.balance -= amount
+                print("Operation successful")
+            else:
+                print("Insufficient funds")
+        else:
+            print("Invalid pin")
+
+    def check_balance(self):
+        temp_pin = input("Enter your pin: ")
+        if temp_pin == self.pin:
+            print("Balance:", self.balance)
+        else:
+            print("Invalid pin")
+
+
+atm = ATM()
